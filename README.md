c90a85b188f956d12a6833b4d27c2523ade1de2e
Andrew Head <head.andrewm@gmail.com>
2016-05-25 14:36:42 -0700

    Logging: Report location events from tabs and windows

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-#Search Task Helper
+# Search Task Helper
+
 A Firefox addon that listens to and reports search behavior.
+
+Note that in the future this project could / should be implemented with the [WebExtensions API](https://developer.mozilla.org/en-US/Add-ons/WebExtensions), which will allow cross-browser compatibility.
+For the sake of convenience and familiarity, I have currently implemented it with the [Firefox Add-on SDK](https://developer.mozilla.org/en-US/Add-ons/SDK).
--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 var self = require('sdk/self');
+var windows = require('sdk/windows').browserWindows;
+var tabs = require('sdk/tabs');
+var Request = require('sdk/request').Request;
 
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
+function log(tab, eventMessage) {
+
+  console.log("Logging event:", eventMessage, "-", tab.index, tab.title, tab.url);
+
+  var request = Request({
+    url: "http://127.0.0.1:8000/api/location_event/",
+    content: JSON.stringify({
+      visit_date: new Date().toISOString(),
+      tab_index: tab.index,
+      title: tab.title,
+      url: tab.url,
+      event_type: eventMessage
+    }),
+    contentType: 'application/json'
+  });
+  request.post();
+
 }
 
-exports.dummy = dummy;
+// Listen for when the window goes in and out of focus
+windows.on('deactivate', function(window) {
+  log(window.tabs.activeTab, "Window deactivated");
+});
+
+windows.on('activate', function(window) {
+  log(window.tabs.activeTab, "Window activated");
+});
+
+// Listen for the loading of new tabs and switching between tabs
+tabs.on('open', function(tab) {
+  log(tab, "Tab opened");
+});
+
+tabs.on('ready', function(tab) {
+  log(tab, "Tab content loaded");
+});
+
+tabs.on('activate', function(tab) {
+  log(tab, "Tab activated");
+});
